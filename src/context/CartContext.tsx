ee6f9782060ac4d95a6e792e49f77edb9c6da3f7
Arthur <whitfielfur@gmail.com>
2026-01-12 01:47:06 +0300

    Update CartContext.tsx

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState, ReactNode } from "react";
-
+import { createContext, useContext, useState } from "react";
+import type { ReactNode } from "react";
 type Product = {
   id: string;
   name: string;
