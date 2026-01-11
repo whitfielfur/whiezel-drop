e80ce85abb12387082c8b113776e2a9eda0857ef
Arthur <whitfielfur@gmail.com>
2026-01-12 01:43:12 +0300

    Update CartContext.tsx

--- src/components/CartContext.tsx
+++ src/components/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState, type ReactNode } from "react"; // Vercel is arguing about that type thingy.
 
 type Product = {
   id: string;
